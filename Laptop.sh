53e4f012702b1748a07f819c5977289c60913f97
Dmitriy Usov <69606738+QapFUc@users.noreply.github.com>
2023-04-07 17:29:11 +0000

    Update Laptop.sh

--- Laptop.sh
+++ Laptop.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+echo "Make 2 partition first for boot and second for root."
+read Yes
 mkfs.vfat /dev/nvme0n1p1
 mkfs.btrfs -f /dev/mapper/cryptdisk
 mount /dev/mapper/cryptdisk /mnt
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
 umount -R /mnt
-mount -o subvol=@,compress=lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt
 mkdir /mnt/boot
 mkdir /mnt/home
 mount /dev/nvme0n1p1 /mnt/boot
-mount -o subvol=@home,compress-lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt/home
 pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd networkmanager mesa-demos btrfs-progs sudo p7zip git vim xorg net-tools iproute2
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
