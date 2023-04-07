1e2234a0f90c953cc487a187bca50dc0ba08e8a0
Dmitriy Usov <69606738+QapFUc@users.noreply.github.com>
2023-04-07 16:16:04 +0000

    Update Laptop.sh

--- Laptop.sh
+++ Laptop.sh
@@ -10,6 +10,6 @@ mkdir /mnt/boot
 mkdir /mnt/home
 mount /dev/nvme0n1p1 /mnt/boot
 mount -o subvol=@home,compress-lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt/home
-pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd networkmanager mesa-demos btrfs-progs sudo p7zip git vim xorg
+pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd networkmanager mesa-demos btrfs-progs sudo p7zip git vim xorg net-tools iproute2
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
