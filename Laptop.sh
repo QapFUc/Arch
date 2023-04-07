ef0cb301396ce69159eebbfb9984f727dae46f83
Dmitriy Usov <69606738+QapFUc@users.noreply.github.com>
2023-04-07 14:03:24 +0000

    Update Laptop.sh

--- Laptop.sh
+++ Laptop.sh
@@ -8,7 +8,7 @@ umount -R /mnt
 mount -o subvol=@,compress=lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt
 mkdir /mnt/boot
 mkdir /mnt/home
-mount /dev/sda1 /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 mount -o subvol=@home,compress-lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt/home
 pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd networkmanager mesa-demos btrfs-progs sudo p7zip git vim xorg
 genfstab -U /mnt >> /mnt/etc/fstab
