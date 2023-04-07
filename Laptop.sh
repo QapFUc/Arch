b8c01fb448e6f2b25214f15db63eefd9a5c596ba
Dmitriy Usov <69606738+QapFUc@users.noreply.github.com>
2023-04-07 09:43:54 +0000

    Update and rename First.sh to Laptop.sh

---
+++ Laptop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkfs.vfar /dev/nvme0n1p1
+mkfs.btrfs -f /dev/mapper/cryptdisk
+mount /dev/mapper/cryptdisk /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+umount -R /mnt
+mount -o subvol=@,compress=lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt
+mkdir /mnt/boot
+mkdir /mnt/home
+mount /dev/sda1 /mnt/boot
+mount -o subvol=@home,compress-lzo,ssd,autodefrag /dev/mapper/cryptdisk /mnt/home
+pacstrap /mnt base base-devel linux linux-headers linux-firmware dhcpcd networkmanager mesa-demos btfs-progs sudo p7zip git vim xorg
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
