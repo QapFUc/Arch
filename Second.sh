b3ebf41b49ca07530769602217eddd330369e41a
Dmitriy Usov <69606738+QapFUc@users.noreply.github.com>
2023-04-09 12:42:50 +0000

    Update Second.sh

--- Second.sh
+++ Second.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cp Arch/locale.gen /etc/locale.gen
+cp /Arch/locale.gen /etc/locale.gen
 locale-gen
-cp Arch/mkinitcpio.conf /etc/mkinitcpio.conf 
+cp /Arch/mkinitcpio.conf /etc/mkinitcpio.conf 
 mkinitcpio -p linux
 bootctl install --path=/boot
-cp Arch/loader.conf /boot/loader/loader.conf
-cp Arch/arch.conf /boot/loader/entries/arch.conf
+cp /Arch/loader.conf /boot/loader/loader.conf
+cp /Arch/arch.conf /boot/loader/entries/arch.conf
 useradd -m -G users,wheel -s /bin/bash qapfuc
 systemctl enable dhcpcd.service NetworkManager
